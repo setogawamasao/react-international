180b3103146ad831a6b5876e10dd073e0b7367e2
masaosetogawa <ms.job.hunting@gmail.com>
2020-05-04 21:42:15 +0900

    Minimum component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { IntlProvider } from "react-intl";
+import AppRoute from "./AppRoute";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<
+  {},
+  { locale: string; messages: Record<string, string> }
+> {
+  constructor(prop: any) {
+    super(prop);
+    this.state = {
+      locale: "en",
+      messages: { greet: "hello" },
+    };
+  }
+
+  render() {
+    return (
+      <IntlProvider locale={this.state.locale} messages={this.state.messages}>
+        <AppRoute />
+      </IntlProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/AppRoute.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormattedMessage, injectIntl } from "react-intl";
+
+const AppRoute = () => {
+  return (
+    <>
+      <FormattedMessage id="greet" />
+    </>
+  );
+};
+
+export default AppRoute;
